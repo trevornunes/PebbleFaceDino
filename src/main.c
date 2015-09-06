4398cace3107466d82717088f7d0e89a8f0cb302
Trevor L Nunes <trevor.nunes@gmail.com>
2015-09-06 00:22:32 -0400

    initial drop.
    works, dino animations.

---
+++ src/main.c
@@ -0,0 +1,140 @@
+#include <pebble.h>
+  
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_msg_layer;
+
+
+static GFont s_time_font;
+static GFont  s_msg_font;
+
+static char s_msg[] = "      ";
+
+#define MAX_MSG_SIZE 10
+#define MAX_MSG_LEN 5
+  
+static char s_ascTable[256];
+
+
+static void update_msg() {  
+  static int idx = 65;  
+  static int charPos = 0;
+  
+  if( idx++ >= 94) 
+    idx = 65;
+  
+  if ( charPos++ >= 2)
+     charPos =0;
+  
+  s_msg[5] = '\0';
+  
+  memcpy( &s_msg[charPos], &s_ascTable[idx], 1);
+  text_layer_set_text(s_msg_layer, s_msg );
+}
+
+static void update_time() {
+  
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+    
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    //Use 2h hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    //Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+  
+  text_layer_set_text(s_time_layer, buffer);  
+}
+
+static void main_window_load(Window *window) {
+  // Create time TextLayer
+  // 5,52,139,50
+  s_time_layer = text_layer_create(GRect(0, 0, 145, 80));
+  s_msg_layer = text_layer_create(GRect(10,81,145,160));
+  
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+  
+  text_layer_set_background_color(s_msg_layer, GColorWhite);
+  text_layer_set_text_color(s_msg_layer, GColorBlack);
+  text_layer_set_text(s_msg_layer,s_msg);
+  
+  //Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_CALC_60));
+  s_msg_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DINO_48));
+
+  //Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  text_layer_set_font(s_msg_layer, s_msg_font);
+  text_layer_set_text_alignment(s_msg_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_msg_layer));
+
+  // Make sure the time is displayed from the start
+  update_time();
+}
+
+static void main_window_unload(Window *window) {
+  //Unload GFont
+  fonts_unload_custom_font(s_time_font);
+  
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_msg_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+  
+static void tick_secs_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_msg();
+}
+
+static void init() {
+  
+  int i = 0;
+  for (i =0; i < 255; i++ ) {
+      s_ascTable[i] = i;
+  }
+  
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+  
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(SECOND_UNIT, tick_secs_handler);
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
